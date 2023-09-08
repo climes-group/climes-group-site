4d17486f06da4ac54e75137c0fa64e6beef7d203
Rob <rob@climesgroup.ca>
2023-09-08 09:50:42 -0400

    added text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,47 +4,36 @@ function About() {
   return (
     <div>
       <p>
-      Climes Group was established in 2023 for the purpose of advancing climate resilience in buildings. 
-      Our climate is changing, but our buildings aren’t. 
+        Climes Group was established in 2023 for the purpose of advancing climate resilience in buildings.
+        Our climate is changing, but our buildings aren’t.
       </p>
       <p>
-      The climate crisis is a critical threat to our building infrastructure. 
-      We need to both <b>adapt</b> to the impacts of climate change and mitigate our emissions as fast as 
-      possible to reduce future impacts. This approach is called climate resilience. 
-      It is the only method we have to prepare ourselves for the future in a resource conscious manner.  
-      The climate resilience approach ensures all our actions achieve both mitigation and adaptation 
-      through life-cycle thinking.
+        The climate crisis is a critical threat to our building infrastructure.
+        We need to both <b>adapt</b> to the impacts of climate change and mitigate our emissions as fast as
+        possible to reduce future impacts. This approach is called climate resilience.
+        It is the only method we have to prepare ourselves for the future in a resource conscious manner.
+        The climate resilience approach ensures all our actions achieve both mitigation and adaptation
+        through life-cycle thinking.
+      </p>
+      <p>
+        To best serve you, Climes Group has 3 divisions:
       </p>
       <Block>
-        <h2>Heading 2-1</h2>
+      <h2>Climes Consulting</h2>
+      <p>
+        Where we provide building engineering consulting services, from climate risk assessments, design development, and forensics
+      </p>
+
+      <h2>Climes Research</h2>
+      <p>
+        Where we address foundational and innovative research questions
+      </p>
+        <h2>Climes Academy</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          sodales, augue faucibus feugiat dapibus, massa lacus aliquam nulla, at
-          viverra nisi ex at tortor. Nam placerat laoreet orci sed egestas.
-          Curabitur in mauris finibus magna euismod volutpat sed a justo. Aenean
-          ullamcorper libero id enim tincidunt ornare. Aenean eu porta nunc. In
-          vulputate mi quis tellus pretium, et tempus ante tristique. Morbi sed
-          tristique tellus, et accumsan massa. Pellentesque euismod augue nunc,
-          vel tincidunt orci rhoncus eu. Nam erat ipsum, vestibulum sed
-          imperdiet in, commodo vestibulum lorem. Praesent non leo eu ipsum
-          rhoncus consectetur at vel metus.
+          Where we translate our expert knowledge into workshops, seminars, and training material
         </p>
       </Block>
-      <Block>
-        <h2>Heading 2-1</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          sodales, augue faucibus feugiat dapibus, massa lacus aliquam nulla, at
-          viverra nisi ex at tortor. Nam placerat laoreet orci sed egestas.
-          Curabitur in mauris finibus magna euismod volutpat sed a justo. Aenean
-          ullamcorper libero id enim tincidunt ornare. Aenean eu porta nunc. In
-          vulputate mi quis tellus pretium, et tempus ante tristique. Morbi sed
-          tristique tellus, et accumsan massa. Pellentesque euismod augue nunc,
-          vel tincidunt orci rhoncus eu. Nam erat ipsum, vestibulum sed
-          imperdiet in, commodo vestibulum lorem. Praesent non leo eu ipsum
-          rhoncus consectetur at vel metus.
-        </p>
-      </Block>
+
     </div>
   );
 }
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,34 +3,38 @@ import Block from "../components/content/Block";
 function Services() {
   return (
     <div>
+      <p>Climes Group adheres to the advancement principle that we need Applied services to understand the problems, Research to solve the problems,
+        and Training to share the solutions.
+        Climes Group has three divisions each tailored to these facets: Climes Consulting, Climes Research, and Climes Academy.</p>
       <Block>
-        <h2>Heading 2-1</h2>
+        <h2>Climes Consulting</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          sodales, augue faucibus feugiat dapibus, massa lacus aliquam nulla, at
-          viverra nisi ex at tortor. Nam placerat laoreet orci sed egestas.
-          Curabitur in mauris finibus magna euismod volutpat sed a justo. Aenean
-          ullamcorper libero id enim tincidunt ornare. Aenean eu porta nunc. In
-          vulputate mi quis tellus pretium, et tempus ante tristique. Morbi sed
-          tristique tellus, et accumsan massa. Pellentesque euismod augue nunc,
-          vel tincidunt orci rhoncus eu. Nam erat ipsum, vestibulum sed
-          imperdiet in, commodo vestibulum lorem. Praesent non leo eu ipsum
-          rhoncus consectetur at vel metus.
+          Climes Consulting provides building engineering consulting services for new and existing buildings.
+          But unlike others, climate resilience is the starting point to all of our work.
+        </p>
+        <p>
+          We provide the standard engineering consulting services of concept review, design development, field review and testing,
+          and forensic investigations. We also provide climate risk assessments, carbon consulting, and life-cycle planning services.
         </p>
       </Block>
       <Block>
-        <h2>Heading 2-1</h2>
+        <h2>Climes Research</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          sodales, augue faucibus feugiat dapibus, massa lacus aliquam nulla, at
-          viverra nisi ex at tortor. Nam placerat laoreet orci sed egestas.
-          Curabitur in mauris finibus magna euismod volutpat sed a justo. Aenean
-          ullamcorper libero id enim tincidunt ornare. Aenean eu porta nunc. In
-          vulputate mi quis tellus pretium, et tempus ante tristique. Morbi sed
-          tristique tellus, et accumsan massa. Pellentesque euismod augue nunc,
-          vel tincidunt orci rhoncus eu. Nam erat ipsum, vestibulum sed
-          imperdiet in, commodo vestibulum lorem. Praesent non leo eu ipsum
-          rhoncus consectetur at vel metus.
+          Climes Research exists to solve problems. We are a think and do tank.
+        </p>
+        <p>
+          We use our  15+ years of academic and industry research experience to ensure the solutions are pragmatic and useful,
+          because unlike other researchers, we’re also practitioners: we know what tools are needed to get the job done and how they are used.
+        </p>
+      </Block>
+      <Block>
+        <h2>Climes Academy</h2>
+        <p>
+          At Climes Academy, we leverage our university, college, and business level training programs to provide impactful, actionable learning.
+          We translate our expert knowledge into workshops, seminars, and training material to train you and your staff to become self-sufficient.
+        </p>
+        <p>
+          "A rising tide lifts all boats"
         </p>
       </Block>
     </div>
