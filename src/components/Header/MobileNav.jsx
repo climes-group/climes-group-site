b0e97f8833e7795087634971fa6181f4a769b9d6
Michael Bieniek <michael.c.bieniek@gmail.com>
2023-09-13 17:20:58 -0400

    Putting back mobile navs

--- src/components/Header/MobileNav.jsx
+++ src/components/Header/MobileNav.jsx
@@ -1,10 +1,14 @@
+import { slide as Menu } from "react-burger-menu";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { PAGES } from "../../utils";
 
 const MobileOnly = styled.div`
   display: none;
   @media (max-width: 767px) {
     display: block;
   }
+  margin-top: -0.5em;
   a {
     color: #fff;
   }
