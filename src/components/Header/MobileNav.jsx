e4e211bf13c288d565f8604b6952a064369f3cdb
Michael Bieniek <michael.c.bieniek@gmail.com>
2023-09-15 08:38:46 -0400

    Header responsive changes

--- src/components/Header/MobileNav.jsx
+++ src/components/Header/MobileNav.jsx
@@ -1,25 +1,98 @@
-import { slide as Menu } from "react-burger-menu";
+import { push as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { PAGES } from "../../utils";
+import { PAGES, THEME } from "../../utils";
 
 const MobileOnly = styled.div`
   display: none;
   @media (max-width: 767px) {
     display: block;
   }
-  margin-top: -0.5em;
+
   a {
     color: #fff;
   }
+
+  /* Position and sizing of burger button */
+  .bm-burger-button {
+    position: absolute;
+    width: 36px;
+    height: 30px;
+    right: 36px;
+    top: 36px;
+  }
+
+  /* Color/shape of burger icon bars */
+  .bm-burger-bars {
+    background: rgb(52, 88, 0);
+  }
+
+  /* Color/shape of burger icon bars on hover*/
+  .bm-burger-bars-hover {
+    background: ${THEME.ACCENT};
+  }
+
+  /* Position and sizing of clickable cross button */
+  .bm-cross-button {
+    height: 24px;
+    width: 24px;
+  }
+
+  /* Color/shape of close button cross */
+  .bm-cross {
+    background: ${THEME.ACCENT};
+  }
+
+  .bm-menu-wrap {
+    position: fixed;
+    height: 100%;
+  }
+
+  /* General sidebar styles */
+  .bm-menu {
+    background: #373a47;
+    padding: 2.5em 1.5em 0;
+    font-size: 1.15em;
+  }
+
+  .bm-item {
+    & :hover {
+      color: ${THEME.ACCENT};
+    }
+    & :active {
+      color: ${THEME.ACCENT};
+    }
+  }
+
+  /* Morph shape necessary with bubble or elastic */
+  .bm-morph-shape {
+    fill: #373a47;
+  }
+
+  /* Wrapper for item list */
+  .bm-item-list {
+    color: #b8b7ad;
+    padding: 0.8em;
+  }
+
+  /* Styling of overlay */
+  .bm-overlay {
+    background: rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const MobileLinkWrapper = styled.div`
+  margin-top: 1em;
 `;
 
 export default function MobileNav() {
   return (
     <MobileOnly>
-      <Menu right>
+      <Menu right noOverlay>
         {PAGES.map((menuItem) => (
-          <Link to={menuItem.url}>{menuItem.label}</Link>
+          <MobileLinkWrapper key={`mobile-menu-${menuItem.label}`}>
+            <Link to={menuItem.url}>{menuItem.label}</Link>
+          </MobileLinkWrapper>
         ))}
       </Menu>
     </MobileOnly>
