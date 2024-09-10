7882882da126861727e8141d61211729267c0bf7
Rob <rob@climesgroup.ca>
2024-09-09 21:09:08 -0400

    More At

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <div>
       <block>
         <h2>Robert Lepage | PhD, P.Eng.</h2>
-        <h3>e: <a href="mailto: rob@climesgroup.ca">rob (at) climesgroup.ca</a></h3>
+        <h3>e: <a href="mailto: rob@climesgroup.ca">rob@climesgroup.ca</a></h3>
         <h3>c: 226 868 9865</h3>
       </block>
     </div>
